804ca93b1821bd54e603c053c929ddda40d5a456
Julien Savary <juliensavary2001@gmail.com>
2024-08-08 09:03:42 -0700

    changes to the website for a simpler ui

---
+++ website/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Privacy from './Privacy';
+
+function App() {
+  return (
+    <div className="App">
+      <Privacy />
+    </div>
+  );
+}
+
+export default App;
